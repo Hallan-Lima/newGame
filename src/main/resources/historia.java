0010e3aba7e9cd7e84b804336d4c5717db57b2d4
Hallan-Lima <hallansrl.t@gmail.com>
2024-01-25 16:36:08 -0300

    jogo teste

---
+++ src/main/resources/historia.java
@@ -0,0 +1,34 @@
+public class historia {
+    public static void roteiro(String[] args, int fase) {
+        switch (fase) {
+            case 1:
+                // A primeira fase se concentrar no desenvolvimento físico
+
+                break;
+
+            case 2:
+                // A segunda fase se concentrar no desenvolvimento intelectual
+
+                break;
+
+            case 3:
+                // E a terceira fase se concentrar no desenvolvimento social
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public static void caminhos(String[] args) {
+        switch (args) {
+            case value:
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ src/main/resources/personagem.java
@@ -0,0 +1,13 @@
+public class personagem {
+    String nome = null;
+    int idade = null;
+    int força = 25;
+    int inteligência = 25;
+    int carisma = 25;
+    int habilidadeFísica = 25;
+    int reputacao = 25;
+
+    public static void main(String[] args) {
+
+    }
+}
